e1a16a0937f95ff764ea3babd2461ef7c61c970e
Al Desrahim <37100012+aldesrahim@users.noreply.github.com>
2022-10-17 09:38:35 +0700

    Create useWaitForElement composable

---
+++ src/composables/useWaitForElement.js
@@ -0,0 +1,20 @@
+/* SOURCE: https://stackoverflow.com/a/61511955/9701449*/
+export function useWaitForElement(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
